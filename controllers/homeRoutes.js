c1cdd2b73bffd533a605735066e5c8f92bfab7dc
isaac long <isaaclong123@outlook.com>
2021-10-16 17:53:11 -0400

    test

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
 
 const main = "../veiws/main.handlebars"
+const index = "./index.handlebars"
+
+const fs  = require("fs")
+
+if (fs.existsSync(index)) {
+    // path exists
+    console.log("exists:", index);
+  } else {
+    console.log("DOES NOT exist:", path);
+  }
 
 router.get('/', (req, res) => {
     //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-    res.render(main, {layout : 'index'});
+    res.render(main, {layout : index});
     });
 
     module.exports = router;
