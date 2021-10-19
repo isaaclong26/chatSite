689d833b521b39d8c0089223e9e15318fde31261
isaac long <isaaclong123@outlook.com>
2021-10-19 16:03:10 -0400

    fixed some code it works kinda

--- public/js/addPost.js
+++ public/js/addPost.js
@@ -1,39 +1,39 @@
 
 
 const addPostHandler = async(event) =>{
-    event.preventDefault();
-    const title = $("#postTitle").val();
-    console.log("ran app post handler")
-    const content = $("#postContent").val()
-    
+  event.preventDefault();
+  const title = $("#postTitle").val();
+  console.log("ran app post handler")
+  const content = $("#postContent").val()
+  
 
-    console.log(title + " " + content);
-    if (title && content) {
-        const response = await fetch(`/api/posts`, {
-          method: 'POST',
-          body: JSON.stringify({
-              "title": title,
-              "content": content,
-              "owner": req.session.user_id
-              
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          
-        });
-    
-        if (response.ok) {
-          document.location.replace('/');
-          alert("made new post")
-        } else {
-          alert('Failed to create project');
-        }
+  console.log(title + " " + content);
+  if (title && content) {
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+            "title": title,
+            "content": content,
+            // "owner": req.session.user_id
+            
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+        alert("made new post")
+      } else {
+        alert('Failed to create project');
       }
-    
-    
+    }
+  
+  
 
-    
+  
 
 }
 
