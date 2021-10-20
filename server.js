a856d28cd623ca0a464ffbde7d851be03d27541a
isaac long <isaaclong123@outlook.com>
2021-10-20 10:18:27 -0400

    changed server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -34,7 +36,6 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
